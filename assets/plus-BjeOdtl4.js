c29e36a81162c74095d3a4e440b457de8ed75bd2
fabioxavierfx <allfiles.cloud@gmail.com>
2026-03-24 08:42:57 -0300

    Updates

---
+++ assets/plus-BjeOdtl4.js
@@ -0,0 +1,1 @@
+import{c as a}from"./index-VKvzRhUk.js";const e=[["path",{d:"M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z",key:"1a8usu"}]],c=a("pen",e);const o=[["path",{d:"M5 12h14",key:"1ays0h"}],["path",{d:"M12 5v14",key:"s699le"}]],t=a("plus",o);export{t as P,c as a};
